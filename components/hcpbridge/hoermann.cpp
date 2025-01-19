e045115b97475593d3ba165142193cf2f9e96e0d
jwkgit <jwtkok@gmail.com>
2025-01-19 22:06:09 +0100

    Update hoermann.cpp

--- components/hcpbridge/hoermann.cpp
+++ components/hcpbridge/hoermann.cpp
@@ -40,7 +40,7 @@ void HoermannGarageEngine::setup(int8_t rx, int8_t tx, int8_t rts)
 {
   RS485.begin(57600, SERIAL_8E1, rx, tx);
   if (rts == -1) {
-    mb.begin(&RS485, rts, true);
+    mb.begin(&RS485, 13, true);
   } else {
     mb.begin(&RS485, rts, true);
   }
